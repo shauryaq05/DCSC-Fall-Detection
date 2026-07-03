baf12a95746cceb9e4ef3ee323a4f1520f0eee71
shauryaq05 <iamshauryasubhra@gmail.com>
2026-07-03 15:59:10 +0530

    Mono repo structure

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import torch
 import numpy as np
 import joblib
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 SAMPLING_LEN = 600
 NUM_CHANNELS = 6
